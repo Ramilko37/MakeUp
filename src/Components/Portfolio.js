8ca108b1a7a028dedbc86b043354fd5770f3539d
Ramilko37 <galyamdin@gmail.com>
2021-03-14 12:49:55 +0300

    Add

---
+++ src/Components/Portfolio.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled, {css} from 'styled-components'
+import { render, screen } from '@testing-library/react';
+import images from "../utils/images";
+
+
+const Portfolio = styled.button`
+    border: 1px solid red;
+    width: 100%;
+  height: auto;
+    `;
+
+<Portfolio></Portfolio>
+
+export default Portfolio;
---
+++ src/Components/Title.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled, {css} from 'styled-components'
+import { render, screen } from '@testing-library/react';
+import images from '../utils/images';
+
+const Title = styled.h3`
+  
+  font-family: Playfair Display;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 40px;
+  line-height: 126.6%;
+  /* or 51px */
+  letter-spacing: 0.01em;
+  /* text/headings */
+  color: #4C4248;
+  margin: ${props => props.titleMargin || ''};
+`;
+
+<Title> </Title>
+
+export default Title;
